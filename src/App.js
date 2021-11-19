7f39b7e8b0c630d936c1fc22056ed7e80eed62f4
HariKharaN <hari88529@gmail.com>
2021-11-20 00:50:04 +0530

    MoviesIMDB app

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Movie from "./Components/Movie";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import MovieRoundedIcon from "@material-ui/icons/MovieRounded";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=d4a2ea6952ee3b388e8f575f8801db1c&query=";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d4a2ea6952ee3b388e8f575f8801db1c&page=1;";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(async () => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  };
+  var result;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+    }
+    setSearchTerm("");
+  };
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  return (
+    <div>
+      <div className="Navbar">
+        <span className="nav-brand">
+          <IconButton>
+            <MovieRoundedIcon style={{ color: "white" }}></MovieRoundedIcon>
+          </IconButton>
+          <a className="logo" href="/">
+            MovieDB
+          </a>
+        </span>
+        <header>
+          <form onSubmit={(setIsLoading, handleSubmit)}>
+            <input
+              type="text"
+              className="search"
+              placeholder="Search..."
+              value={searchTerm}
+              onChange={handleChange}
+            />
+            <span className="search-icon">
+              <IconButton>
+                <SearchIcon
+                  onClick={handleSubmit}
+                  style={{ color: "white" }}
+                ></SearchIcon>
+              </IconButton>
+            </span>
+          </form>
+        </header>
+      </div>
+      {result && (
+        <div className="results-show">Results for {searchTerm} . . .</div>
+      )}
+      <div className="movie-container">
+        {movies.length > 0 ?
+          movies.map((movie) => <Movie key={movie.id} {...movie} />) : <h1>No Movies Found</h1>}
+      </div>
+      <div className="pages">
+        <a>Back</a>
+        <a>Next</a>
+      </div>
+      <footer className="copyright">
+        <span>&copy; 2021 MovieHK</span>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Movie.js
@@ -0,0 +1,38 @@
+import React from "react";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const seVoteClass = (vote) => {
+  if (vote > 8) {
+    return "green";
+  } else if (vote >= 6) {
+    return "orange";
+  } else {
+    return "red";
+  }
+};
+
+const Movie = ({ title, poster_path, overview, vote_average }) => (
+  <div className="movie">
+    <img
+      src={
+        poster_path
+          ? IMG_API + poster_path
+          : "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW92aWV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      }
+      alt={title}
+    />
+    <div className="movie-info">
+      <h5>{title}</h5>
+      <span className={`tag ${seVoteClass(vote_average)}`}>{vote_average}</span>
+    </div>
+    <div className="movie-over">
+      <div className="over-more">
+        <h4 className="over-title">Overview</h4>
+        <button>More</button>
+      </div>
+      <p className="over-text">{overview}</p>
+    </div>
+  </div>
+);
+
+export default Movie;
